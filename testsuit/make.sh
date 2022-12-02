bb5ea5f2bc6793f70b7cc6d3d76c721d01836fa0
Mark <87077242+MarkFedoseev@users.noreply.github.com>
2022-12-03 02:35:36 +0300

    Update make.sh

--- testsuit/make.sh
+++ testsuit/make.sh
@@ -1,1 +1,1 @@
-python operations_test.py ./operations_test.cpp ~/development/projects/netprot/testsuit/bi_in.out ~/development/projects/netprot/testsuit/bi_out.out 
+python operations_test.py ./operations_test.cpp ./bi_in.out ./bi_out.out 
